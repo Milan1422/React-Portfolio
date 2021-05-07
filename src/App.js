2a342fbb019b857f17b80d52dfa64401c60d4614
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-05-07 11:21:17 -0400

    added carousel to jumbotron component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About/About';
 import Jumbotron from './components/Jumbotron/Jumbotron';
 import Navbar from "./components/Navbar/Navbar"
 import Skills from './components/Skills/Skills';
+import TitleMessage from "./components/TitleMessage/TitleMessage"
 import Wrapper from "./components/Wrapper/Wrapper"
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Wrapper> 
       <Navbar/>
       <Jumbotron/>
+      <TitleMessage/>
       <About/>
       <Skills/>
     </Wrapper>
--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Slide1 from "../../assets/img/jumbotron/slide1.webp";
+import Slide2 from "../../assets/img/jumbotron/slide2.webp";
+import Slide3 from "../../assets/img/jumbotron/slide3.webp";
+import ScrollDown from "../ScrollDown/scrollDown";
+import "./Jumbotron.css";
 
 const Jumbotron = () => {
   return (
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">Hello, My name is Jesus Milan</h1>
-        <p class="lead">
-          I am a full stack web developer.
-        </p>
-      </div>
+    <div id="home">
+      <Carousel controls={false} indicators interval={2500} pause={false}>
+        <Carousel.Item>
+          <img className="d-block w-100 custom-img" src={Slide2} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100 custom-img" src={Slide3} alt="Third slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100 custom-img" src={Slide1} alt="Third slide" />
+        </Carousel.Item>
+      </Carousel>
+      <ScrollDown />
     </div>
   );
 };
